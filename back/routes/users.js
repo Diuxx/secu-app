dfa70bcc5b5e0bfb96a25c0ec3b6f9ada170eb3d
Diuxx <nicolasmarmot@gmail.com>
2020-03-11 11:02:56 +0100

    feat(init): create users c(r)ud

--- back/routes/users.js
+++ back/routes/users.js
@@ -8,9 +8,25 @@ const db = require('../database/conf');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // res.send('respond with a resource');
-  var users = db.get('users').value();
-  res.send(users);
+  try {
+    var users = db.get('users').value();
+    res.status(200).send(users);
+  } catch(e) {
+    // --
+    res.status(400).send('Unable to retrieve user list');
+  }
+});
+
+/* GET user by id */
+router.get('/:id', (req, res) => {
+  try {
+    const user = db.get('users').find({ id: parseInt(req.params.id) }).value();
+    if(!user) throw 'id doesn\'t exist';
+    res.status(200).send(user);
+  } catch(e) {
+    // --
+    res.status(400).send('Unable to retrieve user with current id');
+  }
 });
 
 module.exports = router;
