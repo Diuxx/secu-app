8cc7b547b24683871c62da873bfcb9d9377f56d4
Diuxx <nicolasmarmot@gmail.com>
2020-03-12 16:18:36 +0100

    feat(init): app is now initialised

--- secu-app-front/src/app/_components/header/header.component.ts
+++ secu-app-front/src/app/_components/header/header.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
-import { Subscription } from 'rxjs';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
-import { User } from 'src/app/_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,9 +10,16 @@ import { User } from 'src/app/_models/user';
 export class HeaderComponent implements OnInit {
 
   constructor(
-    private auth: AuthenticationService
+    private auth: AuthenticationService,
+    private router: Router
   ) { }
 
   ngOnInit() {
   }
+
+  goHome() {
+    this.auth.logout();
+  }
+
+
 }
--- secu-app-front/src/app/_services/authentication.service.ts
+++ secu-app-front/src/app/_services/authentication.service.ts
@@ -1,4 +1,4 @@
-﻿import { Injectable } from '@angular/core';
+﻿﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -58,6 +58,9 @@ export class AuthenticationService {
         // remove user from loscal storage to log user out
         localStorage.removeItem('currentUser');
         this.currentUserSubject.next(null);
+
+        /* redirect */
+        this.router.navigate(['/login']);
     }
 
 }
